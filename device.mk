0789455c9c9bd95bb4fa0e18f404599853534644
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    wahoo: Declare EUICC permission
    This is a squash of the following:
    
    Author: Ethan Chen <intervigil@gmail.com>
    Date:   Sat Dec 1 13:53:00 2018 -0800
    
        wahoo: Declare EUICC permission
    
        Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Fri Oct 18 00:06:22 2019 -0500
    
        wahoo: Move eUICC permissions xml to /product
    
        All eUICC items were moved to /product in Q.
    
        Change-Id: I614f86b16f61959c47465863ab1d5d55030209ab
    
    Change-Id: I323dce8b848b44e45ed21002950059e58719113c

--- device.mk
+++ device.mk
@@ -363,6 +363,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     move_widevine_data.sh
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # NFC packages
 PRODUCT_PACKAGES += \
     NfcNci \
