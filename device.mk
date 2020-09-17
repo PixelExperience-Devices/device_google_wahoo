e801f4eafbaddbac501e5dd035a3826e70f8bff2
Lunarixus <Nathan@lunarixus.party>
2020-09-17 23:30:16 +0000

    wahoo: Disable privapp permission enforcement
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=disabled
 
 PRODUCT_PACKAGES += \
     messaging
