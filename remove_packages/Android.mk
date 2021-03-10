925f272ab354519774a124a578d32344f549aec2
PixelBoot <bootingpixels@gmail.com>
2021-03-10 17:36:12 +0100

    wahoo: remove_packages: Remove some more apps to fit packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Maps Photos Youtube YoutubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := GoogleTTS CalculatorGooglePrebuilt CalendarGooglePrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
