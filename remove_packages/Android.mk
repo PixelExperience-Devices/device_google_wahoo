1972d774e2953cd2b859f18cc1d495e7e668307f
PixelBoot <bootingpixels@gmail.com>
2021-06-18 23:08:05 +0200

    wahoo: remove_packages: remove further packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := GoogleTTS CalculatorGooglePrebuilt CalendarGooglePrebuilt talkback
+LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt CarrierMetrics DiagnosticsToolPrebuilt GoogleTTS Ornament PrebuiltBugle PrebuiltDeskClockGoogle Tycho talkback
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
