1c4f999f34d12e8d537443e292bf766d67c3a5ba
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-11-06 08:18:11 -0700

    wahoo: Add shim for missing symbol in lib-imsvt.so
    * E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system_ext/lib64/lib-imsvideocodec.so"
    
    Signed-off-by: Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>

--- device.mk
+++ device.mk
@@ -759,6 +759,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     NowPlayingOverlay
 
+# Shims
+PRODUCT_PACKAGES += \
+    lib-imsvtshim
+
 # IORap app launch prefetching using Perfetto traces and madvise
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.iorapd.enable=true
