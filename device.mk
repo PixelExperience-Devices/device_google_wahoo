e74f758a5f36a2d8b10014d499c18e5506fab57b
Sal Savage <salsavage@google.com>
2022-04-07 15:54:04 -0700

    wahoo: Update LE Audio profile properties to match spec and implementation
    Bug: 217448211
    Test: atest BluetoothInstrumentationTests
    Change-Id: I9312b9b70b51603aca6da6f4081533ab73c1614d

--- device.mk
+++ device.mk
@@ -345,7 +345,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.profile.a2dp.source.enabled=true \
     bluetooth.profile.avrcp.target.enabled=true \
     bluetooth.profile.bap.broadcast.assist.enabled=true \
-    bluetooth.profile.bap.unicast.server.enabled=true \
+    bluetooth.profile.bap.unicast.client.enabled=true \
     bluetooth.profile.bas.client.enabled=true \
     bluetooth.profile.csip.set_coordinator.enabled=true \
     bluetooth.profile.gatt.enabled=true \
@@ -360,8 +360,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.profile.pan.panu.enabled=true \
     bluetooth.profile.pbap.server.enabled=true \
     bluetooth.profile.sap.server.enabled=true \
-    bluetooth.profile.tbs.server.enabled=true \
-    bluetooth.profile.vc.server.enabled=true
+    bluetooth.profile.ccp.server.enabled=true \
+    bluetooth.profile.vcp.controller.enabled=true
 
 # Bluetooth SoC
 PRODUCT_PROPERTY_OVERRIDES += \
