12a4196492c9b4085c973539a00e8f0fad92a6fb
PixelBoot <bootingpixels@gmail.com>
2023-02-03 22:49:07 +0100

    Revert "Add recovery.wipe file."
    This reverts commit 1ff004a52906d5c1787d144c463533e0677a99b2.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,6 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_USES_METADATA_PARTITION := true
 
 # Partitions (listed in the file) to be wiped under recovery.
-TARGET_RECOVERY_WIPE := device/google/wahoo/recovery.wipe
 TARGET_RECOVERY_FSTAB := device/google/wahoo/fstab.hardware
 
 BOARD_AVB_ENABLE := true
